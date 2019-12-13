bd7fdf63d3b1a7d0c9dcf558eaf4ad3e4946987a
caiqueubasp <caiqueubasp@gmail.com>
2019-12-13 17:12:23 -0300

    BreakingChocolateProblem

---
+++ JavaScript/BreakingChocolateProblem.js
@@ -0,0 +1,20 @@
+// Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+
+// For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+
+// If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+
+
+var n = 20;
+var m = 15;
+
+function breakChocolate(n,m) {
+    var choco = n*m-1;
+if(choco > 0){
+return choco;
+}else{
+  return 0;
+  }
+  }
+
+  breakChocolate(n,m)
